b2f19e745f046d5c42311008d896081effaa23a3
add <add>
2023-12-01 12:24:26 +0100

    updated config

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 
 # enable kernal modules
 echo "dtoverlay=dwc2" | tee -a /boot/config.txt
-echo "dwc2" | tee -a /etc/modules
+echo "dwc2" | tee -a /etc/modules 
 echo "libcomposite" | tee -a /etc/modules
 
 
 
-# configure device
-mv ./config/isticktoit_usb /usr/bin/isticktoit_usb
-chmod +x /usr/bin/isticktoit_usb
+
+mv ./config/isticktoit_usb /usr/bin/isticktoit_usb # move device configuration script to final location
+chmod +x /usr/bin/isticktoit_usb # make device configuration script executable
+
+mv ./config/rc.local # configure device configuration script to start on startup
+
