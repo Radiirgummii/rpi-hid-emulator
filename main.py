2faac997a99082ebf676de594a61bca3b4b79f63
add <add>
2023-12-05 19:44:09 +0100

    update

--- main.py
+++ main.py
@@ -42,21 +42,6 @@ def hid_encoder(key, modifier=[]):
     write_report(chr(mod) + chr(0) + chr(keys[key]) + chr(0) * 5)
     write_report(chr(0)*8)
     keys ={"a":4}
-    for num,key1 in enumerate("****abcdefghijklmnopqrstuvwxyz1234567890!*'> ß´ü+##öä^,.-ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
-        keys[key1] = num
-
-    mod = 0
-    if "shift" in modifier:
-        mod + 1
-    if "ctrl" in modifier:
-        mod + 2
-    if "alt" in modifier:
-        mod + 4
-    if "gui" in modifier:
-        mod + 8
-    logging.debug(f"encoding key: {key} as {repr(keys[key])}")
-    write_report(chr(mod) + chr(0) + chr(keys[key]) + chr(0) * 5)
-    write_report(chr(0)*8)
 
 def text_encode(text):
     for i in text:
